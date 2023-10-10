9baa1fe7ed0bc802c383eda9a1b51616e7cb1071
Dario Sechi <dario.sechi.rnd@gmail.com>
2023-10-10 21:18:07 +0200

    Add type string to exported constants

--- scripts/generate-exports.mjs
+++ scripts/generate-exports.mjs
@@ -11,5 +11,5 @@ for (let asset of await glob('assets/*.svg')) {
   cname = 'ICON_FEATHER_' + getSnakeCaseString(name).toUpperCase()
   svg = await readFile(asset)
 
-  await appendFile('src/index.ts', `export const ${cname} = \`${svg}\`\n`)
+  await appendFile('src/index.ts', `export const ${cname}: string = \`${svg}\`\n`)
 }
