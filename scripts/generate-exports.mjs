99c3642a27a57d3928d7bbca7f2d68c1ae08afa2
Dario Sechi <dario.sechi.rnd@gmail.com>
2025-04-04 00:46:34 +0200

    Upgrade deps

--- scripts/generate-exports.mjs
+++ scripts/generate-exports.mjs
@@ -1,16 +1,17 @@
 import { getSnakeCaseString } from '@aracna/core'
 import { mkdir, readFile, rm, writeFile } from 'fs/promises'
 import { glob } from 'glob'
+import { basename, extname } from 'path'
 
 await rm('src/assets', { force: true, recursive: true })
 await mkdir('src/assets')
 
-for (let asset of await glob('assets/*.svg')) {
+for (let path of await glob('assets/*.svg')) {
   let name, cname, svg
 
-  name = asset.replace('assets/', '').replace('.svg', '')
+  name = basename(path).replace(extname(path), '')
   cname = 'ICON_F_' + getSnakeCaseString(name).toUpperCase()
-  svg = await readFile(asset)
+  svg = await readFile(path)
 
   await writeFile(`src/assets/${name}.ts`, `export const ${cname}: string = \`${svg}\``)
 }
