3798f620308b4ca5c4f663b52c2952d1e87c181c
Dario Sechi <dario.sechi.rnd@gmail.com>
2025-09-28 10:16:56 +0200

    Build to a single assets file like in material symbols

--- scripts/fns/generate-exports.mjs
+++ scripts/fns/generate-exports.mjs
@@ -1,11 +1,10 @@
 import { getSnakeCaseString } from '@aracna/core'
-import { mkdir, readFile, rm, writeFile } from 'fs/promises'
+import { appendFile, readFile, writeFile } from 'fs/promises'
 import { glob } from 'glob'
 import { basename, extname } from 'path'
 
 export async function generateExports() {
-  await rm('src/assets', { force: true, recursive: true })
-  await mkdir('src/assets')
+  await writeFile('src/assets.ts', '')
 
   for (let path of await glob('assets/*.svg')) {
     let name, cname, svg
@@ -14,6 +13,6 @@ export async function generateExports() {
     cname = 'ICON_F_' + getSnakeCaseString(name).toUpperCase()
     svg = await readFile(path)
 
-    await writeFile(`src/assets/${name}.ts`, `export const ${cname}: string = \`${svg}\``)
+    await appendFile('src/assets.ts', `export const ${cname}: string = \`${svg}\`\n`)
   }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,17 +1,17 @@
 import { getKebabCaseString } from '@aracna/core'
 import { readFile } from 'fs/promises'
 import { describe, expect, it } from 'vitest'
-import * as ICONS from '../src'
+import * as ICONS from '../src/assets'
 
 describe('index', () => {
   it('contains the correct exports', async () => {
-    for (let icon in ICONS) {
+    for (let [name, value] of Object.entries(ICONS)) {
       let fname: string, file: string
 
-      fname = getKebabCaseString(icon.replace('ICON_FEATHER_', '').toLowerCase()) + '.svg'
+      fname = getKebabCaseString(name.replace('ICON_F_', '').toLowerCase()) + '.svg'
       file = await readFile(`./assets/${fname}`, { encoding: 'utf8' })
 
-      expect(file).toBe(ICONS[icon])
+      expect(file).toBe(value)
     }
   })
 })
